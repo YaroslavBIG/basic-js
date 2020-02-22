7d0b79987d99aff9d5a299144b21aa1ae2a9222a
FireGUN <yaroslavbig@ya.ru>
2020-02-23 01:53:05 +0200

    feet: implemet what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,32 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  };
+  if (!date instanceof Date) {
+    throw new Error;
+  };
+
+  const month = date.getMonth();
+  
+  switch(month) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:   
+      return 'spring';
+    case 5:   
+    case 6:   
+    case 7:   
+      return 'summer';
+    case 8:   
+    case 9:   
+    case 10:   
+      return 'autumn';
+    default:
+      throw new Error;
+  };
+
 };
