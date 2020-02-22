95c4fb37262a78fc11a6da156eb60a18085a3165
FireGUN <yaroslavbig@ya.ru>
2020-02-22 22:55:12 +0200

    feet: implemet the countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let counter = 0;
+    for (count = 0; count < matrix.length; count += 1) {
+      const subArr = matrix[count]
+      for (count2 = 0; count2 < subArr.length; count2 += 1) {
+        const cats = '^^';
+        subArr[count2] === cats ? counter += 1 : counter += 0
+      }
+    };
+  return counter;
 };
