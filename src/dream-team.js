e8b73a6102830b48146df1a30434f1168d7b92f9
FireGUN <yaroslavbig@ya.ru>
2020-02-27 00:21:32 +0200

    fix: dream-team and recursive-depth

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,24 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members.length === 0) {
+    throw Error;
+  }
+  const sortedArr = members.sort()
+  let result = ''.toUpperCase();
+  for(const value of sortedArr) {
+    if (typeof value === String) {
+       if (value[0] !== '') {
+         result += value[0];
+       } 
+       else {
+         for (let count = 0; count < value.length; count += 1) {
+           if (value[count] !== '' && value[count -1] === '') {
+             result += value[count];
+             break;
+           }
+         }
+       }
+      }
+      return result;
+    }
+    console.log(result);
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,26 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let result = 1;
+        for (const x of arr) {
+           const isArr = Array.isArray(x) ? true : false;
+            switch(isArr) {
+                case(false):
+                continue;
+            
+                case(true):    
+                result += 1;
+                for (const flat of arr) {
+                    if (Array.isArray(flat)) {
+                        result += 1;
+                        calculateDepth(flat);
+                }
+            }
+            }
+        }
+            return result;
     }
+        
+        
+        
+    
 };
